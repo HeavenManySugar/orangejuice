ce823af92b7a1c9d628f3bde3d415f0dc33221a7
ZRE <chy853@gmail.com>
2025-02-09 02:42:45 +0800

    Add API proxy rewrites and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://server.gitea.orb.local/api/v1/:path*", // Proxy to Backend
+      },
+    ];
+  }
 };
 
 export default nextConfig;
