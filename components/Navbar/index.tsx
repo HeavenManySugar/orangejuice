78549e2ccee473b56e0d629b8a7e7b86ce83bd05
ZRE <chy853@gmail.com>
2025-02-09 11:56:15 +0800

    Refactor Navbar component to use daisyUI for improved styling and remove unnecessary state management

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from 'next/link';
-import { useState } from 'react';
 
 
 const links = [
@@ -10,53 +9,53 @@ const links = [
 ];
 
 const Navbar = () => {
-    const [showMenu, setShowMenu] = useState(false);
-
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
-
     return (
-        <nav className='bg-gray-800'>
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16'>
-                <div className='flex-shrink-0'>
-                    <Link href='/' className='capitalize text-white hover:bg-gray-700 rounded-lg p-2'>
+        <nav className='navbar bg-base-100'>
+            <div className='navbar-start'>
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {links.map(({ href, label }, index) => (
+                            <li key={index}>
+                                <Link href
+                                    ={href} className='capitalize'>
+                                    {label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className='flex-1'>
+                    <Link href='/' className='btn btn-ghost text-xl'>
                         Home
                     </Link>
                 </div>
+            </div>
+            <div className='navbar-end'>
                 <div className='hidden md:block'>
                     {links.map(({ href, label }, index) => (
-                        <Link key={index} href={href} className='capitalize text-white hover:bg-gray-700 rounded-lg p-2'>
+                        <Link key={index} href={href} className='capitalize btn btn-ghost'>
                             {label}
                         </Link>
                     ))}
                 </div>
-                <div className='md:hidden flex items-center'>
-                    <button className='text-white hover:bg-gray-700 rounded-lg p-2' onClick={toggleMenu}>
-                        {showMenu ? (
-                            <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12'></path>
-                            </svg>
-                        ) : (
-                            <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16'></path>
-                            </svg>
-                        )}
-                    </button>
-                </div>
             </div>
-            {showMenu && (
-                <div className='md:hidden'>
-                    <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                        {links.map(({ href, label }, index) => (
-                            <Link key={index} href
-                                ={href} className='capitalize text-white hover:bg-gray-700 rounded-lg p-2'>
-                                {label}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-            )}
         </nav>
     );
 };
