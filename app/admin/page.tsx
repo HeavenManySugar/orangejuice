bf60e3a1bc45b8a5143640f98c7156136c4050df
ZRE <chy853@gmail.com>
2025-02-10 14:11:18 +0800

    Add link to Organizations page in Admin panel for improved navigation

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Admin() {
     return (
         <div>
             <h1>Admin Page</h1>
+            <Link href="/admin/orgs" className="link-primary">Organizations</Link>
         </div>
     );
 };
